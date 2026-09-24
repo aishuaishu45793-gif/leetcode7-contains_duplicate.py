6d64afcd16e3e663e71826f5071bd7ea24bbf745
Aishwarya <aishuaishu45793@gmail.com>
2026-09-24 19:41:55 +0530

    Add Contains Duplicate solution

---
+++ python/leetcode7 contains_duplicate.py
@@ -0,0 +1,18 @@
+class Solution:
+    def containsDuplicate(self, nums):
+        seen = set()
+
+        for num in nums:
+            if num in seen:
+                return True
+
+            seen.add(num)
+
+        return False
+
+
+solution = Solution()
+
+nums = [1, 2, 3, 1]
+
+print(solution.containsDuplicate(nums))
